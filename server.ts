7b1dbf359ec4914b4d2b7826e2bc276cb43324d7
Enzo Lespart <enzolespart@gmail.com>
2018-06-21 21:31:09 +0100

    backend updated

--- server.ts
+++ server.ts
@@ -1,22 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+let mysql = require('mysql');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  console.log('empty get');
+let client = mysql.createConnection({
+  user: 'darkadmin',
+  password: 'aQOTCtLSz8ranLpdQWx0',
+  host: '139.59.155.45',
+  port: '3306',
 });
+ 
+client.query('USE arcadehub');
+
 
 app.post('/login', (req, res) => {
-  console.log(req.body);
   let username = req.body.username;
   let password = req.body.password;
 });
 
 app.post('/score', (req, res) => {
-  let apiKey = req.body.headers.authorization;
+  let apiKey = req.headers.authorization;
+  let gameName = req.body.gameName;
   let score = req.body.score;
+
+  client.query(
+    'INSERT INTO scores (iduser, idgame, score, fecha) values((SELECT id FROM users WHERE apiKey = ?), (SELECT id FROM games WHERE gamename = ?), (?), NOW())', 
+    [apiKey, gameName, score]
+  );
 });
 
 app.listen(3000, () => {
